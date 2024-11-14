dd10e769e43902d818c937ed53cb4e6c7b8366a5
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-11-14 13:23:42 -0300

    Fix: Landing animated words

--- app/(landing)/portfolioLanding/PortfolioLanding.tsx
+++ app/(landing)/portfolioLanding/PortfolioLanding.tsx
@@ -3,6 +3,7 @@ import './PortfolioLanding.scss';
 import TapHint from '@/app/shared/components/tapHint/TapHint';
 import { TbHandClick } from 'react-icons/tb';
 import AnimatedWord from './components/AnimatedWord/AnimatedWord';
+import AnimatedWordD from './components/AnimatedWordD/AnimatedWordD';
 
 export default function PortfolioLanding() {
   const initialTexts = {
@@ -25,25 +26,28 @@ export default function PortfolioLanding() {
       </picture>
       <article>
         <div className="gostContainer"></div>
+        <div className='wordsContainerD'>
+        <AnimatedWordD />
+        </div>
         <div className="wordsContainer">
           <AnimatedWord
             text={initialTexts.text1}
             hoverText="About" // Texto al pasar el ratón
-            animationDelay={300} // Retraso de la animación
+            animationDelay={200} // Retraso de la animación
             href="/about" // Puedes poner la URL aquí si lo deseas
           />
 
           <AnimatedWord
             text={initialTexts.text2}
             hoverText="Projects" // Texto alternativo para hover
-            animationDelay={500} // Retraso de animación
+            animationDelay={300} // Retraso de animación
             href="/projects" // Puedes definir el enlace aquí
           />
 
           <AnimatedWord
             text={initialTexts.text3}
             hoverText="Contact"
-            animationDelay={600} // Retraso de animación
+            animationDelay={400} // Retraso de animación
             href="/contact"
             className="variant"
           />
