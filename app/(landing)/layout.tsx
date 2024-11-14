97aa228ab6d95d4ab4cfbf917f69c7037ae7e45f
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-11-14 13:32:17 -0300

    chore: 🚧SEO refactor

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -10,7 +10,11 @@ export const metadata: Metadata = {
   keywords: ["Frontend Developer", "JavaScript", "React", "Web Development", "Portfolio", "Alejandro"],
   icons: {
     icon: "/images/LogoAle3D.webp",
-  }
+  },
+  robots: {
+    index: true, // Asegura que Google indexe tu sitio
+    follow: true, // Permite que los motores de búsqueda sigan los enlaces internos
+  },
 };
 
 export default function RootLayout({
