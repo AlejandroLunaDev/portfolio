d6bb57e2c9cfb97a847054030d798d9ac8dc2c63
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-11-15 09:48:57 -0300

    Refactor:🚧 landing and svgs

--- app/(landing)/portfolioLanding/PortfolioLanding.tsx
+++ app/(landing)/portfolioLanding/PortfolioLanding.tsx
@@ -2,15 +2,12 @@ import Image from 'next/image';
 import './PortfolioLanding.scss';
 import TapHint from '@/app/shared/components/tapHint/TapHint';
 import { TbHandClick } from 'react-icons/tb';
-import AnimatedWord from './components/AnimatedWord/AnimatedWord';
+
 import AnimatedWordD from './components/AnimatedWordD/AnimatedWordD';
+import Link from 'next/link';
 
 export default function PortfolioLanding() {
-  const initialTexts = {
-    text1: 'Hello',
-    text2: 'I am',
-    text3: 'Ale',
-  };
+
 
   // Puedes definir hoverText y animationDelay en cada palabra.
   return (
@@ -25,35 +22,17 @@ export default function PortfolioLanding() {
         />
       </picture>
       <article>
-        <div className="gostContainer"></div>
+
         <div className='wordsContainerD'>
         <AnimatedWordD />
+        <div className='links-menu'>
+          <Link className='word1' href={'/about'}>about</Link>
+          <Link className='word2' href={'/projects'}>projects</Link>
+          <Link className='word3' href={'/contact'}>contact</Link>
         </div>
-        <div className="wordsContainer">
-          <AnimatedWord
-            text={initialTexts.text1}
-            hoverText="About" // Texto al pasar el ratón
-            animationDelay={200} // Retraso de la animación
-            href="/about" // Puedes poner la URL aquí si lo deseas
-          />
-
-          <AnimatedWord
-            text={initialTexts.text2}
-            hoverText="Projects" // Texto alternativo para hover
-            animationDelay={300} // Retraso de animación
-            href="/projects" // Puedes definir el enlace aquí
-          />
-
-          <AnimatedWord
-            text={initialTexts.text3}
-            hoverText="Contact"
-            animationDelay={400} // Retraso de animación
-            href="/contact"
-            className="variant"
-          />
         </div>
       </article>
-      <TapHint Icon={TbHandClick} paragraphText="Tap anywhere" />
+      <TapHint Icon={TbHandClick} paragraphText="Tap anyword" />
     </section>
   );
 }
